9a10e15a65e74fea059471df9ccd3610346105e7
yarmolovichalex <yarmolovichalex@gmail.com>
2016-07-27 00:27:54 +0300

    angular addArtist form

---
+++ MusicStore/MusicStore.Web/Scripts/Controllers/AdminController.js
@@ -0,0 +1,13 @@
+﻿angular
+    .module('musicStoreApp')
+    .controller('AdminController', [
+        '$scope', '$http', function($scope, $http) {
+
+            $scope.submit = function() {
+                $http.post('/musicstore/admin/addartist', {
+                    Name: $scope.name,
+                    Country: $scope.country
+                });
+            }
+        }
+    ]);
