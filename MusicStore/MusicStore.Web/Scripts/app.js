fbc91e7ac92aafd7ee96b38596d18e52f2307300
yarmolovichalex <yarmolovichalex@gmail.com>
2016-07-31 14:17:22 +0300

    ng-view on admin screen

--- MusicStore/MusicStore.Web/Scripts/app.js
+++ MusicStore/MusicStore.Web/Scripts/app.js
@@ -1,1 +1,21 @@
-﻿angular.module('musicStoreApp', []);
+﻿angular.module('musicStoreApp', ['ngRoute'])
+    .config([
+        '$routeProvider', function($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: '../scripts/templates/add-artist.html',
+                    controller: 'AdminController',
+                    controllerAs: 'ctrl'
+                })
+                .when('/artist', {
+                    templateUrl: '../scripts/templates/add-artist.html',
+                    controller: 'AdminController',
+                    controllerAs: 'ctrl'
+                })
+                .when('/album', {
+                    templateUrl: '../scripts/templates/add-album.html',
+                    controller: 'AdminController',
+                    controllerAs: 'ctrl'
+                });
+        }
+    ]);
